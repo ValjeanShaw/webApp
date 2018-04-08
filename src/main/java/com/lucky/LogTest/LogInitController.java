d3df706a10dde758544d48d8492dd7d2b1f44c9d
xiaoran <xiaoran737@163.com>
2018-04-08 09:40:02 +0800

    1.日志集成 2.mybatis预准备

--- src/main/java/com/lucky/LogTest/LogInitController.java
+++ src/main/java/com/lucky/LogTest/LogInitController.java
@@ -1,18 +1,26 @@
 package com.lucky.LogTest;
 
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
-@RequestMapping("")
+@RequestMapping("/Log")
 public class LogInitController {
 
-    @RequestMapping(value = "/")
-    public void LogPrint(){
-        Integer integer = Integer.valueOf(3);
+    private static final Logger LOGGER = LoggerFactory.getLogger(LogInitController.class);
 
+    @RequestMapping(value = "/initLog")
+    @ResponseBody
+    public String LogPrint(){
+        LOGGER.info("日志请求  {}","begin");
 
-
+        return "log end";
 
     }
 
