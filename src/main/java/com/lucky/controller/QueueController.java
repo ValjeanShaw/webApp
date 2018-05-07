e356a63ffcc8e1b83803a5e4d077478f48046490
xiaoran <xiaoran737@163.com>
2018-05-07 20:13:05 +0800

    rabbitmq整理

--- src/main/java/com/lucky/controller/QueueController.java
+++ src/main/java/com/lucky/controller/QueueController.java
@@ -1,17 +1,18 @@
 package com.lucky.controller;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.annotation.Resource;
 
 @Controller
 @RequestMapping("/queue")
 public class QueueController {
 
-    @Resource(name = "sendNoticeTemplate")
+    @Autowired
+    @Qualifier("sendNoticeTemplate")
     private RabbitTemplate messProducer;
 
 
